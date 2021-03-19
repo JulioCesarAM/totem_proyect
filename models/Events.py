51e517a3737700ea6e44406d6d03203c27e215ac
JulioCesarAM <59057978+JulioCesarAM@users.noreply.github.com>
2021-03-19 16:18:29 +0000

    last changes
    durisimo

--- models/Events.py
+++ models/Events.py
@@ -2,16 +2,16 @@
 
 from odoo import models, fields, api, _
 
-class Event(models.Model):
-     _name = 'event.totem'
-     name = fields.Text(sstring=_(''))
-     title = fields.Text(string=_(''))
-     bannerImg = fields.Binary(string=_(''))
-     sliderImg = fields.One2many('slider.totem', 'event_id_fk', string=_(''))
-     description = fields.Text(string=_(''))
-     footer = fields.Text(string=_(''))
-     qr = fields.Text()
 
-     #https://www.odoo.com/documentation/14.0/reference/qweb.html
-    
+class Event(models.Model):
+    _name = 'event.totem'
+    name = fields.Text(sstring=_(''))
+    title = fields.Text(string=_(''))
+    bannerImg = fields.Binary(string=_(''))
+    sliderImg = fields.One2many('slider.totem', 'event_id_fk', string=_(''))
+    description = fields.Text(string=_(''))
+    footer = fields.Text(string=_(''))
+    qr = fields.Text()
+
+
 
