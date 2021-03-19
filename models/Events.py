48279dced65dddf6ff4563fa2a2eb6de63f2f3c1
JulioCesarAM <59057978+JulioCesarAM@users.noreply.github.com>
2021-03-19 09:31:03 +0000

    ultimos cambios
    fix

--- models/Events.py
+++ models/Events.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class Event(models.Model):
      _name = 'event.totem'
-     name = fields.Text(string='')
-     title = fields.Text(string='')
-     bannerImg = fields.Binary(string='')
-     sliderImg = fields.One2many('slider.totem', 'event_id_fk', string='')
-     description = fields.Text(string='')
-     footer = fields.Text(string='')
+     name = fields.Text(sstring=_(''))
+     title = fields.Text(string=_(''))
+     bannerImg = fields.Binary(string=_(''))
+     sliderImg = fields.One2many('slider.totem', 'event_id_fk', string=_(''))
+     description = fields.Text(string=_(''))
+     footer = fields.Text(string=_(''))
      qr = fields.Text()
     
 
--- models/SliderImages.py
+++ models/SliderImages.py
@@ -1,9 +1,9 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class image_slider(models.Model):
     _name = 'slider.totem'
-    name = fields.Text(string='')
+    name = fields.Text(string=_(''))
     image_rute = fields.Binary()
-    event_id_fk = fields.Many2one(compute='generate_fk',comodel_name='event.totem', string='Anuncio')
+    event_id_fk = fields.Many2one(comodel_name='event.totem', string=_('Anuncio'))
     
     
