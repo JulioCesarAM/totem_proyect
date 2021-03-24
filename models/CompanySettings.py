49006b1d9bf724e14ee703f4d6cbcc5f8c65e9a5
JulioCesarAM <59057978+JulioCesarAM@users.noreply.github.com>
2021-03-24 13:33:42 +0000

    implementado el modelo de configuracion
    sin fallos esta vez

--- models/CompanySettings.py
+++ models/CompanySettings.py
@@ -1,11 +1,11 @@
 from odoo import fields,models,_,api,exceptions
 class mainConfigValues(models.Model):
     _inherit='res.company'
-    mainSlider = fields.Float(store="true")
-    secundarySlider = fields.Float(store="true")
-    description = fields.Text(store="true")
-    companyQr = fields.Text(store="true")
-    
+    mainSlider = fields.Float()
+    secundarySlider = fields.Float()
+    description = fields.Text()
+    companyQr = fields.Text()
+    prueba = fields.Integer(string='')
 
 
 
--- models/totem_project_res_config.py
+++ models/totem_project_res_config.py
@@ -1,11 +1,12 @@
 from odoo import fields,models,_,api,exceptions
 class TotemConfigSettings(models.TransientModel):
     _inherit='res.config.settings'
-    main_slider_control = fields.Float(string=_('main slider control'),related="company_id.mainSlider")
-    secundary_slider_control=fields.Float(string=_(''),related="company_id.secundarySlider")
-    description = fields.Text(string=_(''),related="company_id.description")
-    companyQr = fields.Text(string=_(''),related="company_id.companyQr")
-    @api.constrains('main_slider_control')
+    main_slider_control = fields.Float(string=_('main slider control'),related="company_id.mainSlider",readonly=False)
+    secundary_slider_control=fields.Float(string=_(''),related="company_id.secundarySlider",readonly=False)
+    description = fields.Text(string=_(''),related="company_id.description",readonly=False)
+    companyQr = fields.Text(string=_(''),related="company_id.companyQr",readonly=False)
+    prueba = fields.Integer(string='',related="company_id.prueba",readonly=False)
+    @api.constrains('main_slider_control')  
     def _constrains_main_slider_control(self):
         if self.main_slider_control<1:
             raise ValidationError(_("el valor debe ser superior a uno"))
