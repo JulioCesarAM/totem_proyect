aba6416dafddbd826c3d008231f17630c6f5eced
JulioCesarAM <59057978+JulioCesarAM@users.noreply.github.com>
2021-03-25 10:14:11 +0000

    cambios visuales modelo
    cambios visuales en el modelo

--- models/Events.py
+++ models/Events.py
@@ -5,19 +5,12 @@ from odoo import models, fields, api, _
 
 class Event(models.Model):
     _name = 'event.totem'
-    name = fields.Text(string=_(''))
     title = fields.Text(string=_(''))
     bannerImg = fields.Binary(string=_(''))
     sliderImg = fields.One2many('slider.totem', 'event_id_fk', string=_(''))
     description = fields.Text(string=_(''))
-    footer = fields.Text(string=_(''))
     qr = fields.Text()
   
     
-    #@api.depends('')
-    #def _compute_sliderImgTimer(self):
-        #self.sliderImgTimer=self.env['ir.config_parameter'].sudo().get_param('totem_proyect.totem_proyect_img_slider_control')
-        #pass
-
 
 
