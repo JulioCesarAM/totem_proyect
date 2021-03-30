3f6e6795306f0748db13ccabfea9d297c1b682ab
JulioCesarAM <59057978+JulioCesarAM@users.noreply.github.com>
2021-03-30 14:20:58 +0100

    cambios funcionales
    no funciona

--- models/Events.py
+++ models/Events.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _,exceptions
+import logging
 
-
+_logger=logging.getLogger(__name__)
 class Event(models.Model):
     _name = 'event.totem'
     title = fields.Text(string=_(''))
@@ -11,23 +12,53 @@ class Event(models.Model):
     description = fields.Text(string=_(''))
     qr = fields.Text()
     bannerPrincipalSelector = fields.Selection([
-        ('imagen', 'img'),
-        ('video', 'vid')
+        ('img', 'Imagen'),
+        ('vid', 'Subir video'),
+        ('lvid','Url video')
         #('rss_video', 'rssv'),
         #('rss_datos', 'rssd'),
-    ], string=_(''))
-    audioField = fields.Binary(string='')
-    videoField = fields.Binary(string='')
+    ], string=_(''),default='img')
+    audioField = fields.Binary(string=_(''))
+    videoField = fields.Binary(string=_(''))
+    urlVid = fields.Text(string=_(''))
     #RSS video
     #RSS datos
-    @api.multi
     def write(self, vals):
-        if self.bannerPrincipalSelector=='img':
-            self.videoField = False
-        elif self.bannerPrincipalSelector=="vid":
-            self.bannerImg = False
-         
-        return super().write(vals)
+        #records = self.env['event.totem'].write({
+         #   'description' : 'holita'
+
+        #})
+        #_logger.info("500 self = :" + str(self.description))
+        _logger.info("%s 500",vals)
+    
+        #self.write({
+        #    'description' : 'hola'
+        #})
+      
+        if vals['bannerPrincipalSelector']=='img':
+            vals['description']="imagen"
+        else
+            vals['description']="nada"
+
+        record = super(Event,self).write(vals)
+        _logger.info("500 " + str(vals))
+        #_logger.info("%s 500",record)
+        #return record
+        # va 
+        return record
+
+        #if record['bannerPrincipalSelector']=='img':
+            #record['videoField']=False
+            #record['urlVid']=False
+        #elif record['bannerPrincipalSelector']=="vid":
+            #record['bannerImg']=False
+            #record['urlVid']=False
+        #elif record['bannerPrincipalSelector']=="lvid":
+            #record['bannerImg']=False
+            #record['videoField']=False
+        #print("entro")
+
+
   
 
     @api.constrains('description')
