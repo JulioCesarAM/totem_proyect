14301be79809ada33016d96421524d82273faefd
JulioCesarAM <59057978+JulioCesarAM@users.noreply.github.com>
2021-04-08 12:01:41 +0100

    separado datetime
    separado en campo fecha inicio/ fin y hora inicio/fin

--- models/Events.py
+++ models/Events.py
@@ -3,6 +3,7 @@
 from odoo import models, fields, api, _, exceptions
 import logging
 import re
+from datetime import *
 
 _logger = logging.getLogger(__name__)
 
@@ -24,9 +25,11 @@ class Event(models.Model):
     audioField = fields.Binary(string=_(''))
     videoField = fields.Binary(string=_(''))
     urlVid = fields.Text(string=_(''))
-    fechaInicio = fields.Datetime(string=_(''))
-    fechaFin = fields.Datetime(string=_(''))
+    horaInicio = fields.Float(string=_(''))
+    horaFin = fields.Float(string=_(''))
     urlWeb = fields.Text(string=_(''))
+    fechaInicio = fields.Date(string=_(''))
+    fechaFin = fields.Date(string=_(''))
     # RSS video
     # RSS datos
     urlVidId = fields.Text(string=_(''))
@@ -39,11 +42,37 @@ class Event(models.Model):
     def get_events(self, uid):
         
         events_ids = self.env['totem.controllers'].search_read([('admin','=',uid)])
-        events = self.env['event.totem'].search_read([('id','in',events_ids[0]['events'])],['title','sliderImg','description','qr','bannerPrincipalSelector','urlVid','fechaInicio','fechaFin','urlWeb','urlVidId','descriptionPopUp','titlePopUp'])
+        events = self.env['event.totem'].search_read([('id','in',events_ids[0]['events'])],[
+            'title','sliderImg','description','qr',
+            'bannerPrincipalSelector','urlVid','horaInicio'
+            ,'horaFin','fechaInicio','fechaFin','urlWeb'
+            ,'urlVidId','descriptionPopUp','titlePopUp'])
+        #events[0]['title']=self.timeToMiliCovnerter(events[0]['fechaInicio'])
 
+    
+      
+
+
+
+
+        _logger.info(str(events[0]['fechaInicio'])+" 500")
         #aux = self.env['event.totem'].search_read(['id','in',auxId])
-
+        #2021 |- 04 | - 06
         return events
+    
+    #def timeToMiliCovnerter(self,date):
+       # dayString=date.rsplit('-',1)[0]
+       # dayInteger=int(dayString)
+        #dayInteger+=self.leapYear(dayInteger)
+    
+        #aux=date.fromisoformat(str(date))
+        #return aux.timestamp()*1000
+       # pass
+
+    #def leapYear(self,date):
+     #   FINAL_DATE=1972
+      #  return int(((date-FINAL_DATE)/4)+1)
+       # pass
 
     @api.onchange('urlVid', 'urlVidId')
     def _onchange_(self):
